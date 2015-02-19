2c5a964698f6a6261a01f06bd4e27cdb961a6b9e
dmorris4 <dmorris4@binghamton.edu>
2015-02-19 17:17:53 -0500

    Finished the Lab

--- lab4.pl
+++ lab4.pl
@@ -25,12 +25,13 @@ listOfTerms([],_,[]).
 listOfTerms([H|T],Name,ListOfResults) :- X =.. [Name|H], listOfTerms(T,Name,Var), ListOfResults = [X|Var].
 
 /*
+5:
 A BST is a Collection of Nodes originating from a Head node
 Each node can have left and/or right children
 Left children must be lesser than their parent,
 Right Children must be greater than their parent
 */
-isBST(T) :- isBST(T,Min,Max).
+isBST(T) :- isBST(T,_,_).
 isBST(node(V,empty,empty),V,V).
 isBST(node(V,L,empty),Min,Max) :-
 	isBST(L,Lmin,Lmax),
@@ -49,4 +50,30 @@ isBST(node(V,L,R),Min,Max) :-
 	isBST(R,Rmin,Rmax),
 	V < Rmin,
 	Max = Rmax.
+
+insert(E,L1,L2) :- select(E,L2,L1).
+
+applyList([]).
+applyList([H|T]) :- H,applyList(T).
+
+/*
+10:
+Because A is inside a not, it does not unify with anything. Therefore,
+A unifies with B, and B unifies with what, so A unifies with what aswell
+*/
+
+convertToDecimal(A,0).  
+convertToDecimal(succ(S), Y) :- numeral(S), Y1 is Y-1, convertToDecimal(S, Y1). 
+numeral(0). 
+numeral(succ(X)) :- numeral(X).
+
+/*
+11:
+The execution of Y1 is Y-1 needs to happen after the recursive call to convertToDecimal,
+otherwise the data will not be correctly written, and Y will always be 0.
+*/
+
+
+
+
 
